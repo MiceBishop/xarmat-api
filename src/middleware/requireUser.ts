c23883685e5e190d7771c56f4f46e84984936544
Papa Seynou Sy NDIAYE <seynousy@gmail.com>
2022-07-29 16:28:44 +0000

    send unauthorized when no logged user

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -2,9 +2,8 @@ import { NextFunction, Request, Response } from 'express';
 
 export function requireUser(req: Request, res: Response, next: NextFunction) {
   const { user } = res.locals;
-
   if (!user) {
-    return res.sendStatus(403);
+    return res.sendStatus(401);
   }
 
   return next();
